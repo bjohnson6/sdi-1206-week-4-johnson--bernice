780041f7b1be3f12925464390852ef8437a22a5d
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-18 19:29:43 -0400

    Changes to number 1. validate a phone number

--- sdi-project4/johnson_bernice_assigment4.js
+++ sdi-project4/johnson_bernice_assigment4.js
@@ -4,4 +4,21 @@
 //Deliverable 4
 
 
-//1# 
+//1# string
+
+//Does a string follow a 123-456-7890 pattern like a phone number? //keep working on this
+function validatePhoneNum(number){  //validates the number 
+    var phoneNumPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;  //the regEx code
+      return phoneNumPattern.test(number); //returns true
+
+};   //need to add if else statement
+   
+
+ 
+ validatePhoneNum(6102865545); //number I used to verify number//returned true //call function
+
+//validatePhoneNum(100000000000); // number I used to verify number//returned false
+//finshed 1#  
+
+
+//2#  
