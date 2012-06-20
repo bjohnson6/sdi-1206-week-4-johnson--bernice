265bc21a4705129e6be2d93fc108eaf14f330b52
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-20 14:25:16 -0400

    Put my var myLibrary = function() { in my code. I also put 1# does a string follow a 123-456-7890 pattern like a phone number? Added the return function wrapper. Added var newLib = new myLibrary(); first console.log (newLib.validatePhoneNum(610-2865545));

--- sdi-project4/johnson_bernice_assigment4.js
+++ sdi-project4/johnson_bernice_assigment4.js
@@ -1,24 +1,44 @@
 //alert("JavaScript works!");
 //Bernice Johnson
 //June 18, 2012
-//Deliverable 4
+//Deliverable 4, My Library
 
 
-//1# string
+var myLibrary = function(){
 
-//Does a string follow a 123-456-7890 pattern like a phone number? //keep working on this
-function validatePhoneNum(number){  //validates the number 
-    var phoneNumPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;  //the regEx code
-      return phoneNumPattern.test(number); //returns true
+    //1# string
+    //Does a string follow a 123-456-7890 pattern like a phone number? //keep working on this
+   var validatePhoneNum = function(number){  //validates the number 
+      var phoneNumPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;  //the regEx code
+        return phoneNumPattern.test(number); //returns true
 
-};   //need to add if else statement
+   };   //need to add if else statement????
    
+    //validatePhoneNum(6102865545); //number I used to verify number//returned true //call function
 
- 
- validatePhoneNum(6102865545); //number I used to verify number//returned true //call function
-
-//validatePhoneNum(100000000000); // number I used to verify number//returned false
-//finshed 1#  
+    //validatePhoneNum(100000000000); // number I used to verify number//returned false
+    //finshed 1#  
 
 
-//2#  
+
+
+
+
+//RETURN FUNCTION WRAPPER
+    return {
+    	"validatePhoneNum":validatePhoneNum, //1# return
+
+    	
+    };
+
+
+}; 
+
+var newLib = new myLibrary();
+
+//console.log(newLib.checkNum(15)); // WIMBA DEMONSTRATION FOR REFERENCE
+console.log(newLib.validatePhoneNum(6102865545)); //returns true
+
+
+
+
