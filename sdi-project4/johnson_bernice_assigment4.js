a0812ac605460da4df7c59775aff8c3e12a9904b
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-20 23:21:25 -0400

    Added #2 Does a string follow an aaa@bbb,ccc pattern like and email address? Fixed some errors and added the if/else conditional. tested my email address bernicejohnson@fullsail.edu and it return true and Your email address is valid. I also tested a false email it returned the false message and false. Going to test some more. Finshed and works right now.

--- sdi-project4/johnson_bernice_assigment4.js
+++ sdi-project4/johnson_bernice_assigment4.js
@@ -8,26 +8,35 @@ var myLibrary = function(){
 
     //1# string
     //Does a string follow a 123-456-7890 pattern like a phone number? //keep working on this
-   var validatePhoneNum = function(number){  //validates the number 
+   var validatePhoneNum = function(number){  // Function and  number argument ..validates the number 
       var phoneNumPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;  //the regEx code
         return phoneNumPattern.test(number); //returns true
 
    };   
    
-    //validatePhoneNum(6102865545); //number I used to verify number//returned true //call function
+ //validatePhoneNum(6102865545); //number I used to verify number//returned true //call function
 
-    //validatePhoneNum(100000000000); // number I used to verify number//returned false
-    //finshed 1# ///////////////////////////////////////////////////////////////
+ //validatePhoneNum(100000000000); // number I used to verify number//returned false
+ //finshed 1# ///////////////////////////////////////////////////////////////
 
     //#2 Does a string follow an aaa@bbb.ccc pattern like an email address?
-    var validateEmailAddress = function (email){ //validate the email address
-    	var emailAddressPattern = ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b;
-    	return emailAddressPattern.test(email);
+   var validateEmailAddress = function (email){ // Funtion and string argument ..validate the email address
+      var emailAddressPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; //email regEx code
+    	  if(emailAddressPattern.test(email) === false) {
+	      console.log("Your email address is not valid."); //this message shows up if the email is invalid or incorrect.
+	      return false;
+	  }else{
+		  console.log("Your email address is valid."); //MESSAGE SHOWS UP IF THE EMAIL ADDRESS IS CORRECT.
+		  return true;//  RETURNS TRUE
+      }
+   };
+    	 //used "bernicejohnson@fullsail.edu" to test returns: Your email address is valid. & true.
+    	 //used "bernicejohnson@fullsail.e" to test returns : Your email address is not valid. & false.
+    
+//SAVE TO TEST AGAIN ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;
 
-    //NEED TO WORK ON THIS MORE
-
-
-    };
+//FINISHED 2# /////////////////////////////////////////////////////////////
+  
 
 
 
@@ -37,7 +46,7 @@ var myLibrary = function(){
 //RETURN FUNCTION WRAPPER
     return {
     	"validatePhoneNum":validatePhoneNum, //1# return
-    	"validateEmailAddress":validateEmailAddress,
+    	"validateEmailAddress":validateEmailAddress, //2# return
 
     	
     };
@@ -49,7 +58,18 @@ var newLib = new myLibrary();
 
 //console.log(newLib.checkNum(15)); // WIMBA DEMONSTRATION FOR REFERENCE
 console.log(newLib.validatePhoneNum(6102865545)); // NUMBER I USED TO VERIFY..returns true
-console.log(newLib.validateEmailAddress(bernicejohnson@fullsail.edu)); //EMAIL ADDRESS I USED TO VERIFY..
+console.log(newLib.validateEmailAddress("bernicejohnson@fullsail.edu")); //EMAIL ADDRESS I USED TO VERIFY..
+
+
+
+
+//email verification notes below
+///^[a-zA-Z0-9._-]+:  This means that the email address must begin with alpha-numeric characters (both lowercase and uppercase characters are allowed). It may have periods,underscores and hyphens.
+
+//[a-zA-Z0-9.-]+: Behind the ‘@’ sign there must be some of the alpha-numeric characters. It can also contain period (‘.’) and and hyphens(‘-’).
+//
+//[a-zA-Z]{2,4}$/: The email address must end with two to four alphabets. Having a-z and A-Z means that both lowercase and uppercase letters are allowed. 
+
 
 
 
